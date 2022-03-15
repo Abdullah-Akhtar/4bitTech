81a5ba94b145b51aaae00e2936cca0fcad88bb83
Abdullah Akhtar <mak.abdullah07@gmail.com>
2022-03-15 20:18:31 +0500

    Changing

--- index.php
+++ index.php
@@ -11,18 +11,27 @@
     <title>4Bit Tech</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
-                <!-- Navbar-LOGO -->
-                <a class="navbar-brand btn btn-primary" href="index.php">4Bit Tech</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <!-- Navbar-LOGO -->
+                <a class="navbar-brand" href="index.php">
+                    <img class="rounded-circle" style="box-shadow: 1px 1px 20px black;" 
+                    src="logo.png" alt="" alt="" width="30" height="24">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+                aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <!-- Navbar-Toggler -->
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <ul class="nav justify-content-end">
+            <!-- Navbar-Toggler -->
+            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                <div>
+                <ul class="nav navbar-nav me-auto mb-2 mb-lg-0 d-flex align-items-center ">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Home</a>
+                        <a class="nav-link active" href="example.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Project</a>
@@ -33,20 +42,26 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#">Reviews</a>
                     </li>
+                    <li class="nav-link">
+                        <button class="btn btn-primary" class="subcribe">
+                            Subcribe
+                        </button>
+                    </li>
                 </ul>
+                </div>
             </div>
         </div>
     </nav>
 
     <!-- Body Start -->
-    <div class="container mt-3">
+    <div class="container mt-5">
         <div class="row">
-            <div class="col-lg-6 left">
+            <div class="col-lg-6 col-md-6 left">
                 <div>
                     <h1>4Bit Tech</h1>
                 </div>
                 <div>
-                    <p>
+                    <p class="text-justify text-muted">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
@@ -57,16 +72,41 @@
                     <button class="btn btn-success"> Explore </button>
                 </div>
             </div>
-            <div class="col-lg-6">
+            <div class="col-lg-6 col-md-6 left">
                 <img src="Home.jpg" alt="4Bit Tech" class="img-fluid">
             </div>
         </div>
-
+        <!-- Services -->
+        <div class="row mt-5">
+            <div class="col-md-4 col-sm-12">
+                <h1 class="text-center">Service</h1>
+                <p class="text-justify text-muted">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <h1 class="text-center">Service</h1>
+                <p class="text-justify text-muted">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <h1 class="text-center">Service</h1>
+                <p class="text-justify text-muted">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+            </div>
+        </div>
         <!-- Project -->
-        <div class="row">
-                <h1>Projects</h1>
-                <div class="col-lg-4 col-md-6 col-sm-12 mt-2">
-                    <div class="card" style="width: 18rem;">
+        <div class="row mt-5">
+                <div class="heading">
+                    <h2>Projects</h2>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 mt-3">
+                    <div class="card container" style="width: 18rem;">
                         <img src="card (2).jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
@@ -76,8 +116,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 mt-2">
-                    <div class="card" style="width: 18rem;">
+                <div class="col-lg-4 col-md-6 col-sm-12 mt-3">
+                    <div class="card container" style="width: 18rem;">
                         <img src="card (2).jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
@@ -87,8 +127,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 mt-2">
-                    <div class="card" style="width: 18rem;">
+                <div class="col-lg-4 col-md-6 col-sm-12 mt-3">
+                    <div class="card container" style="width: 18rem;">
                         <img src="card (2).jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
@@ -98,8 +138,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 mt-2">
-                    <div class="card" style="width: 18rem;">
+                <div class="col-lg-4 col-md-6 col-sm-12 mt-3">
+                    <div class="card container" style="width: 18rem;">
                         <img src="card (2).jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
@@ -109,8 +149,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 mt-2">
-                    <div class="card" style="width: 18rem;">
+                <div class="col-lg-4 col-md-6 col-sm-12 mt-3">
+                    <div class="card container" style="width: 18rem;">
                         <img src="card (2).jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
@@ -120,8 +160,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 mt-2">
-                    <div class="card" style="width: 18rem;">
+                <div class="col-lg-4 col-md-6 col-sm-12 mt-3">
+                    <div class="card container" style="width: 18rem;">
                         <img src="card (2).jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
@@ -132,15 +172,54 @@
                     </div>
                 </div>
         </div>
-        <div class="row">
-            <div class="col">
-                
+        
+        <!-- Teams -->
+        <div class="row mt-5">
+            <div class="heading col-lg-12">
+                    <h2>
+                        Team
+                    </h2>
+                </div>
+            <div class="col-lg-2"></div>
+            <div class="col-lg-8">
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img src="card (4).jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="card (4).jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="card (4).jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
             </div>
+            <div class="col-lg-2"></div>
         </div>
     </div>
-        
-    <scrip src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    <div class="row">
+        <div class="">
+            <footer>
+                <div class="footer-copyright text-center py-3">© 2019 Copyright:
+                    <a href="#"> 4bitTech.com</a>
+                </div>
+            </footer>
+        </div>
+    </div>
+
+
+    <script src="bootstrap.bundle.js">
+
     </script>
 </body>
 </html>
